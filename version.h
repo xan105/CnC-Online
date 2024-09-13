5b10552c4b258241b30575d63d87d781bdddab0a
Anthony Beaumont <dev.xan105@outlook.be>
2024-09-13 13:47:00 +0700

    Update version.h

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 	#define VERSION_H
 
-	#define VER_FILEVERSION             1,0,0,0
-	#define VER_FILEVERSION_STR         "1.0.0.0\0"
+	#define VER_FILEVERSION             1,1,0,0
+	#define VER_FILEVERSION_STR         "1.1.0.0\0"
 
-	#define VER_PRODUCTVERSION          1,0,0,0
-	#define VER_PRODUCTVERSION_STR      "1.0.0.0\0"
+	#define VER_PRODUCTVERSION          1,1,0,0
+	#define VER_PRODUCTVERSION_STR      "1.1.0.0\0"
 
 	#define VER_COMPANYNAME_STR         "Xan"
 	#define VER_FILEDESCRIPTION_STR     "Open C&C:Online client"
