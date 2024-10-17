ac57f8cb76f1ace625d246e20d958638f7a089cd
Anthony Beaumont <dev.xan105@outlook.be>
2024-10-17 12:22:51 +0700

    split

--- dllmain.cpp
+++ dllmain.cpp
@@ -195,45 +195,7 @@ struct hostent* WSAAPI detourGetHostByName(const char *name) {
     return pGetHostByName(host.c_str());
 }
 
-bool ReadMemory(HANDLE processHandle, LPCVOID baseAddress, SIZE_T size, std::vector<BYTE>& buffer) {
-    buffer.resize(size);
-    SIZE_T bytesRead;
-    return ReadProcessMemory(processHandle, baseAddress, buffer.data(), size, &bytesRead) && bytesRead == size;
-}
 
-bool FindBytesInMemory(const std::vector<BYTE>& memory, const std::vector<BYTE>& pattern, SIZE_T& foundOffset) {
-    auto it = std::search(memory.begin(), memory.end(), pattern.begin(), pattern.end());
-
-    if (it != memory.end()) {
-        foundOffset = std::distance(memory.begin(), it);
-        return true;
-    }
-    return false;
-}
-
-bool WriteBytesToMemory(HANDLE processHandle, LPVOID baseAddress, const std::vector<BYTE>& newPattern, SIZE_T offset) {
-    // Calculate the target address
-    BYTE* targetAddress = (BYTE*)baseAddress + offset;
-
-    // Change the protection of the memory region
-    DWORD oldProtect;
-    SIZE_T size = newPattern.size();
-    if (!VirtualProtectEx(processHandle, targetAddress, size, PAGE_EXECUTE_READWRITE, &oldProtect)) {
-        std::cerr << "Failed to change memory protection." << std::endl;
-        return false;
-    }
-
-    // Write to the memory
-    bool writeSuccess = WriteProcessMemory(processHandle, targetAddress, newPattern.data(), newPattern.size(), NULL);
-
-    // Restore the old protection
-    if (!VirtualProtectEx(processHandle, targetAddress, size, oldProtect, &oldProtect)) {
-        std::cerr << "Failed to restore memory protection." << std::endl;
-        return false;
-    }
-
-    return writeSuccess;
-}
 
 bool ModifyPublicKey() {
 
